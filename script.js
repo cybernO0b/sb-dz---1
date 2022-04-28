e0994389bd011270a48d295d543889c0e7f7a1e2
cybernO0b <92986665+cybernO0b@users.noreply.github.com>
2022-04-28 22:08:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Фича написанная не собственноручно
+
+function loadData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, 2000);
+    })
+  }
+  
+  loadData()
+    .then(() => {
+      let preloaderEl = document.getElementById('preloader');
+      preloaderEl.classList.add('hidden');
+      preloaderEl.classList.remove('visible');
+    });
+
+
+
+
+
+
+
+
